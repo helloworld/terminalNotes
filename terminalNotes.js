31f5db387baa188c0832975fced61adca9c4a63a
Sashank Thupukari <sthupukari@gmail.com>
2014-10-08 10:53:47 -0400

    Remove Meteor Startup

--- terminalNotes.js
+++ terminalNotes.js
@@ -12,9 +12,6 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
     Router.map(function() {
 
         /////////////////////
