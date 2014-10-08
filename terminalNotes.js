e2ba39c5526466e84a838156503bddef0f1e1351
Sashank Thupukari <sthupukari@gmail.com>
2014-10-08 10:53:11 -0400

    Add New Note
    path: /createNote/:username/:password/:title/:text

--- terminalNotes.js
+++ terminalNotes.js
@@ -49,4 +49,51 @@ if (Meteor.isServer) {
                 this.response.end("User was successfully created");
             }
         });
+
+        //////////////////
+        // ADD NEW NOTE //
+        //////////////////
+        this.route('addNote', {
+            where: 'server',
+            path: '/createNote/:username/:password/:title/:text',
+
+            action: function() {
+                var username = this.params.username;
+                var password = CryptoJS.MD5(this.params.password).toString()
+                var title = this.params.title;
+                var text = this.params.text;
+
+                var currentClient = Clients.findOne({
+                    username: username
+                });
+                if (currentClient.auth == password) {
+                    var note = Notes.findOne({
+                        userName: username,
+                        title: title
+                    });
+                    if (note == undefined) {
+                        Notes.insert({
+                            userName: username,
+                            title: title,
+                            text: text
+                        });
+                        this.response.writeHead(200, {
+                        'Content-Type': 'text/html'
+                        });
+                        this.response.end("New note " + title + " successfully created");
+                    } else {
+                        this.response.writeHead(200, {
+                            'Content-Type': 'text/html'
+                        });
+                        this.response.end("NOTE WITH THIS TITLE ALREADY EXISTS. PLEASE USE DIFFERENT NOTE TITLE");
+                    }                    
+                } else {
+                    this.response.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    this.response.end("ERROR. INCORECT PASSWORD.");
+                }
+            }
+        });
+
 }
