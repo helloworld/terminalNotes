5545ea11c1bbd33438d93b8bfbc4cdb039793b86
Sashank Thupukari <sthupukari@gmail.com>
2014-10-08 10:51:44 -0400

    Notes Display Functionality

--- terminalNotes.js
+++ terminalNotes.js
@@ -1,19 +1,14 @@
+Notes = new Meteor.Collection("notes");
+Clients = new Meteor.Collection("clients");
+
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+    Template.notesList.notes = function() {
+        return Notes.find();
     }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
