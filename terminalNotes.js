feb423895027016044e46ef4491a3c88b3f06806
Sashank Thupukari <sthupukari@gmail.com>
2014-10-08 10:52:29 -0400

    Create New User
    path: /createUser/:username/:password

--- terminalNotes.js
+++ terminalNotes.js
@@ -15,4 +15,38 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+    Router.map(function() {
+
+        /////////////////////
+        // CREATE NEW USER //
+        /////////////////////
+        this.route('createUser', {
+            where: 'server',
+            path: '/createUser/:username/:password',
+
+            action: function() {
+                var username = this.params.username;
+                var password = CryptoJS.MD5(this.params.password).toString()
+
+                if (Clients.fetch({
+                        username: "username"
+                    }).length == undefined) {
+                    Clients.insert({
+                        username: username,
+                        auth: password
+                    });
+                    console.log("USER " + username + " CREATED");
+                } else {
+                    console.log(Clients.fetch({
+                        username: "username"
+                    }))
+                    console.log("USER ALREADY EXISTS");
+                }
+
+                this.response.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                this.response.end("User was successfully created");
+            }
+        });
 }
